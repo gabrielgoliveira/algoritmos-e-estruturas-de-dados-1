1df9e2e5dc1d41fefbcdbb7cf07cb6db77585759
Gabriel G. Oliveira <gabriel.golvs@gmail.com>
2018-06-10 23:17:28 -0300

    limpando o codigo

--- lista-6-filas-pilhas/ex5.c
+++ lista-6-filas-pilhas/ex5.c
@@ -28,6 +28,7 @@ void ordernarCrescente( int *vetor, const int SIZE ){
             }
         }
     }
+    return ;
 }
 
 void ordernarDecrescente( int *vetor, const int SIZE ){
@@ -41,18 +42,12 @@ void ordernarDecrescente( int *vetor, const int SIZE ){
             }
         }
     }
-
+    return ;
 }
 
 int joao( int *dificuldade, int maxTempo, int SIZE ) {
     int i, j, somatoria = 0, cont = 0;
-    /*
-    printf("Ordem Joao: ");
-    for( i = 0; i<SIZE; i++ ){
-        printf("%d ", dificuldade[i]);
-    }
-    printf("\n");
-    */
+    
     for( i = 0; i<SIZE; i++){
         if(somatoria + dificuldade[i] <= maxTempo){
             somatoria+=dificuldade[i];
@@ -62,7 +57,7 @@ int joao( int *dificuldade, int maxTempo, int SIZE ) {
     }
     exercicios_total[0] = i;
     somatoria = 0;
-    //printf("Joao fez %d Exercicios\n", exercicios_total[0]);
+
     for( i = 0; i < exercicios_total[0]; i++ ){
         cont++;
         for( j = 0; j<cont; j++){
@@ -70,20 +65,14 @@ int joao( int *dificuldade, int maxTempo, int SIZE ) {
         }
     }
     pontuacao_geral[0] = somatoria;
-    //printf("Sua Pontuacao Geral foi: %d\n", pontuacao_geral[0]);
+    return 0;
 }
 
 int jose( int *dificuldade, int maxTempo, int SIZE ) {
     //resolve na ordem crescente de dificuldade
     int i, j, somatoria = 0, cont = 0;
     ordernarCrescente( dificuldade, SIZE );
-  /*  
-    printf("Ordem Jose: ");
-    for( i = 0; i<SIZE; i++ ){
-        printf("%d ", dificuldade[i]);
-    }
-    printf("\n");
- */
+
     for( i = 0; i<SIZE; i++){
         if(somatoria + dificuldade[i] <= maxTempo){
             somatoria+=dificuldade[i];
@@ -93,7 +82,6 @@ int jose( int *dificuldade, int maxTempo, int SIZE ) {
     }
     exercicios_total[1] = i;
     somatoria = 0;
-    //printf("Jose fez %d Exercicios\n", exercicios_total[1]);
     for( i = 0; i < exercicios_total[1]; i++ ){
         cont++;
         for( j = 0; j<cont; j++){
@@ -101,20 +89,13 @@ int jose( int *dificuldade, int maxTempo, int SIZE ) {
         }
     }
     pontuacao_geral[1] = somatoria;
-    //printf("Sua Pontuacao Geral foi: %d\n", pontuacao_geral[0]);
+    return 0;
 }
 
 int joaquim( int *dificuldade, int maxTempo, int SIZE ) {
     //resolve na ordem crescente de dificuldade
     int i, j, somatoria = 0, cont = 0;
     ordernarDecrescente( dificuldade, SIZE );
-    /*    
-    printf("Ordem Joaquim: ");
-    for( i = 0; i<SIZE; i++ ){
-        printf("%d ", dificuldade[i]);
-    }
-    printf("\n");
-    */
     for( i = 0; i<SIZE; i++){
         if(somatoria + dificuldade[i] <= maxTempo){
             somatoria+=dificuldade[i];
@@ -124,7 +105,6 @@ int joaquim( int *dificuldade, int maxTempo, int SIZE ) {
     }
     exercicios_total[2] = i;
     somatoria = 0;
-    //printf("Jose fez %d Exercicios\n", exercicios_total[1]);
     for( i = 0; i < exercicios_total[2]; i++ ){
         cont++;
         for( j = 0; j<cont; j++){
@@ -132,53 +112,37 @@ int joaquim( int *dificuldade, int maxTempo, int SIZE ) {
         }
     }
     pontuacao_geral[2] = somatoria;
-    //printf("Sua Pontuacao Geral foi: %d\n", pontuacao_geral[0]);
+    return 0;
 }
 int empateX(){
     int i, j, menorPontuacao = 999999999, auxMenor, indiceSaida = 0;
     int aux = pontuacao_geral[0], contadorEmpate = 0;
-    /*preenchendo uma matriz onde suas posições são elemento i que empatou com elemento j 
-    em relação a exercicios totais realizados*/
     for( i = 1; i<3; i++){
         //verificando se existe empate na pontuacao
         if(aux == pontuacao_geral[i]){
             contadorEmpate++;
         }
     }
-    //printf("Contador Empate = %d\n", contadorEmpate);
     if(contadorEmpate == 2){
-        //printf("Condicao Empate por Pontuacao\n");
+        //empate por pontuação
         printf("%s ganha, com %d problemas resolvidos e pontuacao de %d.\n", nome[1], exercicios_total[1], pontuacao_geral[1]);
         return 1;
     }
 
     //se caso a pontuacao geral nao tiver em empate
+    //utilizando de matriz por adjancencia para achar um vencedor
     for( i=0; i<3; i++){
         for( j=0; j<3; j++){
             if(i != j && exercicios_total[i] == exercicios_total[j]){
                 matrizEmpate[i][j] = 1;
             }
         }
-    }
-    /*
-    printf("MATRIZ DE ADJACENCIA\n");
-    for( i=0; i<3; i++){
-        for( j=0; j<3; j++){
-            printf("%d ", matrizEmpate[i][j]);
-        }
-        printf("\n");
-    }
-    */  
+    }  
     /*Comparando a matriz de adjacencia e obtendo o menor valor da pontuação geral e o indice*/
     for( i=0; i<3; i++){
         for( j=0; j<3; j++){
-            if(matrizEmpate[i][j] == 1){
-                //comparando os dois carinhas
-                //printf("Ocorreu empate entra %s e %s\n", nome[i], nome[j]);
-                //printf("Pontuacao %s = %d e menor que Pontuacao de %s = %d ?\n", nome[i], pontuacao_geral[i], nome[j], pontuacao_geral[j]);
-                if(pontuacao_geral[i] < pontuacao_geral[j] ) {
-                    //printf("SIM\n");
-                    //pontuação i é menor que j
+            if(matrizEmpate[i][j] == 1){ //se houver empate entre i e j
+                if(pontuacao_geral[i] < pontuacao_geral[j] ) { //se a pontuação de i for menor que j, obter pontuação
                     auxMenor = pontuacao_geral[i];
                     if(menorPontuacao > auxMenor ){
                         menorPontuacao = auxMenor;
@@ -189,8 +153,7 @@ int empateX(){
         }
     }
     printf("%s ganha, com %d problemas resolvidos e pontuacao de %d.\n", nome[indiceSaida], exercicios_total[indiceSaida], pontuacao_geral[indiceSaida]);
-
-    //printf("Menor Pontuacao: %d, indice Saida: %d\n", menorPontuacao, indiceSaida);
+    return 1;
 }
 
 void ganhador() {
